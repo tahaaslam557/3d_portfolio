61be1df1e6dda92fdc82f703ad5324725eaf171f
Taha Aslam <tahaaslam557@gmail.com>
2026-06-18 04:38:28 +0500

    Portfolio Project First Commit

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const words = [
+  { text: 'Ideas', imgpath: '/images/ideas.svg'},
+  { text: 'Concepts', imgpath: '/images/concepts.svg'},
+  { text: 'Designs', imgpath: '/images/designs.svg'},
+  { text: 'Code', imgpath: '/images/code.svg'},
+  { text: 'Ideas', imgpath: '/images/ideas.svg'},
+  { text: 'Concepts', imgpath: '/images/concepts.svg'},
+  { text: 'Designs', imgpath: '/images/designs.svg'},
+  { text: 'Code', imgpath: '/images/code.svg'},
+];
